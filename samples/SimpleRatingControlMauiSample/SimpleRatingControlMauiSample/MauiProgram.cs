10ace5bf8160dd61c0465b05f83d1d6fd216a35d
Sebastian Jensen <apps@tsjdev-apps.de>
2022-08-03 19:04:50 +0200

    Register SimpleRatingControlMaui in MauiProgram.

--- samples/SimpleRatingControlMauiSample/SimpleRatingControlMauiSample/MauiProgram.cs
+++ samples/SimpleRatingControlMauiSample/SimpleRatingControlMauiSample/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace SimpleRatingControlMauiSample;
+﻿using SimpleRatingControlMaui;
+
+namespace SimpleRatingControlMauiSample;
 
 public static class MauiProgram
 {
@@ -7,6 +9,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.UseSimpleRatingControl()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
