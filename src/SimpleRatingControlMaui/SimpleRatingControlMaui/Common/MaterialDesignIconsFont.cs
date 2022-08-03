27d380df4e346437a8c6eec789e8e989f62e6759
Sebastian Jensen <apps@tsjdev-apps.de>
2022-08-03 15:27:38 +0200

    Add common files like MaterialDesignIconsFont, RatingType or State.

---
+++ src/SimpleRatingControlMaui/SimpleRatingControlMaui/Common/MaterialDesignIconsFont.cs
@@ -0,0 +1,20 @@
+﻿namespace SimpleRatingControlMaui;
+
+internal class MaterialDesignIconsFont
+{
+    public const string StarOutline = "\U000f04d2";
+    public const string StarHalfFull = "\U000f04d0";
+    public const string Star = "\U000f04ce";
+
+    public const string CircleOutline = "\U000f0766";
+    public const string CircleHalfFull = "\U000f1396";
+    public const string Circle = "\U000f0765";
+
+    public const string HeartOutline = "\U000f02d5";
+    public const string HeartHalfFull = "\U000f06de";
+    public const string Heart = "\U000f02d1";
+
+    public const string ShieldOutline = "\U000f0499";
+    public const string ShieldHalfFull = "\U000f0780";
+    public const string Shield = "\U000f0498";
+}
